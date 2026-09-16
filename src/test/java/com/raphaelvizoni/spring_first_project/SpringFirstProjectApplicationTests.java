834915c403eca9f0e0c04d54ef0969dedde9b71a
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-16 15:47:41 -0300

    Project created

---
+++ src/test/java/com/raphaelvizoni/spring_first_project/SpringFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelvizoni.spring_first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
