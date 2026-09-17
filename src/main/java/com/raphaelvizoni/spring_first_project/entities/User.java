cb433130b51098b1c31ac72cd4533a2eaca6c8fd
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-17 09:09:35 -0300

    H2 database, test profile, jpa

--- src/main/java/com/raphaelvizoni/spring_first_project/entities/User.java
+++ src/main/java/com/raphaelvizoni/spring_first_project/entities/User.java
@@ -1,10 +1,16 @@
 package com.raphaelvizoni.spring_first_project.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-incremento
     private Long id;
     private String name;
     private String email;
