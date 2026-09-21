644e9726d5b23effe2d67fd29275bd9f3b74978a
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-21 13:13:40 -0300

    orderStatus enum

--- src/main/java/com/raphaelvizoni/spring_first_project/entities/Order.java
+++ src/main/java/com/raphaelvizoni/spring_first_project/entities/Order.java
@@ -2,6 +2,7 @@ package com.raphaelvizoni.spring_first_project.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.raphaelvizoni.spring_first_project.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,6 +20,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -26,17 +30,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
-        this.client = client;
-    }
-
-    public User getClient() {
-        return client;
-    }
-
-    public void setClient(User client) {
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -55,6 +52,25 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(this.orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
 
     @Override
     public boolean equals(Object o) {
