b262ce211e803e591517f7145183dd01091eb193
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-22 10:31:00 -0300

    OrderItem, many-to-many association with extra attributes Parte 1

---
+++ src/main/java/com/raphaelvizoni/spring_first_project/entities/OrderItem.java
@@ -0,0 +1,74 @@
+package com.raphaelvizoni.spring_first_project.entities;
+
+import com.raphaelvizoni.spring_first_project.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Double price, Integer quantity) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Order getOrder() {
+        return this.id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        this.id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return this.id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        this.id.setProduct(product);
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
